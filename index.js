18934fccd2d301625fe615e6e5cafee093ec08b0
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-05-02 12:18:19 -0600

    additional functionality added. Archive.zip not updated

--- index.js
+++ index.js
@@ -6,14 +6,32 @@ const db = massive.connectSync({
 });
 
 var handlersInventory= {
-  'IncrementLemons': function(){
-    db.run("update inventory set quantity = (quantity + 1) where productname = 'lemons'", (err,result) => {
+  'Increment': function(item, num){ // Changed IncrementLemons to Increment
+    db.run("update inventory set quantity = (quantity + $2) where productname = $1", [item, num], (err,result) => {
+      if(err){ //In the line above, item correlates to $1 and num correlates to $2 because of their placement in the passed in array.
+        console.log(err)
+      }
+      this.emit(':tell', "I added " + num + item + " guys. Be more grateful. This is hard work.")
+    }); //In the line above, num and item are being passed in from the parameters of the 'Increment' function.
+  }
+
+  'Decrement': function(item, num){ // Copied from Increment. Check notes above for more detailed info.
+    db.run("update inventory set quantity = (quantity - $2) where productname = $1", [item, num], (err,result) => {
       if(err){
         console.log(err)
       }
-      this.emit(':tell', "I added one to lemons guys.")
+      this.emit(':tell', "I subtracted " + num + item + " guys. What is wrong with you? You're losing merch!")
     });
+  }
 
+  'GetInventory': function(item){
+    db.run("select quantity from inventory where productname = $1", [item], (err,result) => {
+      const num = result.data.quantity;
+      if(err){
+        console.log(err)
+      }
+      this.emit(':tell', "You have " + num + item + " brother.")
+    });
   }
 }
 
