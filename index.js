1740ae18cdb6f321cef9c9020c22316981ec8006
Timothy Daniel Mitchell <m.timothyd@gmail.com>
2017-05-02 15:59:09 -0600

    fix parameters

--- index.js
+++ index.js
@@ -5,24 +5,27 @@ const db = massive.connectSync({
   connectionString : "postgres://"+process.env.dbUsername+":"+process.env.dbPassword+"@"+process.env.dbEndpoint,
 });
 
+//this.event.request.intent.slots.item.value
+
 var handlersInventory= {
   'Increment': function(item, num){ // Changed IncrementLemons to Increment
-    db.run("update inventory set quantity = (quantity + $2) where productname = $1", [item, num], (err,result) => {
+    const slots = this.event.request.intent.slots;
+    db.run("update inventory set quantity = (quantity + $2) where productname = $1", [, num], (err,result) => {
       if(err){ //In the line above, item correlates to $1 and num correlates to $2 because of their placement in the passed in array.
         console.log(err)
       }
-      this.emit(':tell', "I added " + num + item + " guys. Be more grateful. This is hard work.")
+      this.emit(':tell', "I added " + num + " " + item + " guys. Be more grateful. This is hard work.")
     }); //In the line above, num and item are being passed in from the parameters of the 'Increment' function.
-  }
+  },
 
-  'Decrement': function(item, num){ // Copied from Increment. Check notes above for more detailed info. 
+  'Decrement': function(item, num){ // Copied from Increment. Check notes above for more detailed info.
     db.run("update inventory set quantity = (quantity - $2) where productname = $1", [item, num], (err,result) => {
       if(err){
         console.log(err)
       }
-      this.emit(':tell', "I subtracted " + num + item + " guys. What is wrong with you? You're losing merch!")
+      this.emit(':tell', "I subtracted " + num + " " + item + " guys. What is wrong with you? You're losing merch!")
     });
-  }
+  },
 
   'GetInventory': function(item){
     db.run("select quantity from inventory where productname = $1", [item], (err,result) => {
@@ -30,7 +33,7 @@ var handlersInventory= {
       if(err){
         console.log(err)
       }
-      this.emit(':tell', "You have " + num + item + " brother.")
+      this.emit(':tell', "You have " + num + " " + item + " brother.")
     });
   }
 }
